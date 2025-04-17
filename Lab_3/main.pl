f59fa698634df8894114546e2b9ed543fe75776b
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-17 11:15:44 +0300

    9. Prolog app small update

--- Lab_3/main.pl
+++ Lab_3/main.pl
@@ -1,6 +1,30 @@
 :- use_module(library(clpfd)).
+% Constraint Logic Programming library
 
 solve_crypto(WordsList, Solution) :-
+   
+    %   Pentru exemplul [send, more, money]:
+    %           - AddendsList = [send, more]
+    %           - Result = money
+    %       Letters = [s,e,n,d,m,o,r,y]
+    %       Values = [Vs,Ve,Vn,Vd,Vm,Vo,Vr,Vy]
+    %       Solution = [s-Vs, e-Ve, n-Vd, d-Vd, m-Vm, o-Vo, r-Vr, y-Vy]
+    %       Constrangeri adaugate: 
+    %               - variabilele intre 0-9
+    %               - prima litera din cuvant nu poate fi 0
+    %       "send" -> Vs*1000+Ve*100+Vn*10+Vd
+    %       Verif ecuatia: (Vs*1000+Ve*100+Vn*10+Vd) + (Vm*1000+Vo*100+Vr*10+Ve) = (Vm*10000+Vo*1000+Vn*100+Ve*10+Vy)
+    %       Asignare valida :
+    %           Solution found:
+                    % s = 9
+                    % e = 5
+                    % n = 6
+                    % d = 7
+                    % m = 1
+                    % o = 0
+                    % r = 8
+                    % y = 2, verifica suma: 9567+1085 = 10652
+
     append(AddendsList, [Result], WordsList),
     
     collect_letters(WordsList, Letters),
@@ -20,22 +44,32 @@ solve_crypto(WordsList, Solution) :-
     
     sum(Addends, #=, ResultNumber),
     
-    labeling([ff], Values).
+    labeling([ff], Values). % [ff] -> euristica: first-fail, ajuta la gasirea solutiei mai repede
 
 collect_letters(Words, UniqueLetters) :-
+    % Toate literele distincte din toate cuvintele
+    % De ex pentru [send, more, money]:
+    %       - [s, e, n, d, m, o, r, y]
     maplist(atom_chars, Words, CharsList),
     append(CharsList, AllChars),
     list_to_set(AllChars, UniqueLetters).
 
+
+
 constrain_first_letters(Words, Solution) :-
+    % Numerele nu incep cu 0
     maplist(constrain_first_letter(Solution), Words).
 
+
+
 constrain_first_letter(Solution, Word) :-
+    % Extrage primul caracter si il constrange sa nu fie 0
     atom_chars(Word, [FirstChar|_]),
     member(FirstChar-Value, Solution),
     Value #\= 0.
 
 word_to_number(Solution, Word, Number) :-
+    % [send] -> [s, e, n, d] -> [1, 2, 3, 4] -> 1234
     atom_chars(Word, Chars),
     maplist(char_to_digit(Solution), Chars, Digits),
     foldl(add_digit, Digits, 0, Number).
@@ -54,7 +88,7 @@ show_numeric_solution(WordsList, Solution) :-
     append(AddendsList, [Result], WordsList),
     maplist(word_to_numeric_display(Solution), AddendsList, NumericAddends),
     word_to_numeric_display(Solution, Result, NumericResult),
-    format('Equation: ', []),
+    format('Sum: ', []),
     print_list_with_plus(NumericAddends),
     format(' = ~w~n', [NumericResult]).
 
@@ -69,14 +103,15 @@ print_list_with_plus([X]) :- !, format('~w', [X]).
 print_list_with_plus([X|Xs]) :- format('~w + ', [X]), print_list_with_plus(Xs).
 
 crypto_solve :-
-    write('Enter cryptarithmetic puzzle as a list (e.g., [send,more,money]): '),
+    write('Short example of correct input: [word1,word2,word3,result]. (dont forget the ".") \n'),
+    write('Enter cryptarithmetic puzzle as a list: '),
     read(WordsList),
     (solve_crypto(WordsList, Solution) ->
         nl, write('Solution found:'), nl,
         print_solution(Solution),
         nl, show_numeric_solution(WordsList, Solution)
     ;
-        nl, write('No solution found.')
+        nl, write('Failed boss')
     ).
     
 :- initialization(crypto_solve).
